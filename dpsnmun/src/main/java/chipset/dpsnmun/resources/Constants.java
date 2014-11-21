a48a4c9fa3d43d20e5471d41da63b50203c9e51d
chipset95 <chipset95@yahoo.co.in>
2014-11-21 22:17:24 +0530

    Complete makeover

---
+++ dpsnmun/src/main/java/chipset/dpsnmun/resources/Constants.java
@@ -0,0 +1,10 @@
+package chipset.dpsnmun.resources;
+
+/*
+ * Developer: chipset
+ * Package : chipset.dpsnmun.resources
+ * Project : DPSNMUN'14
+ * Date : 6/11/14
+ */
+public class Constants {
+}
